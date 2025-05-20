b1e515454b026f7cdb97ededbde9076bb6e7e24d
unknown <reetikacute23@gmail.com>
2025-05-20 13:21:11 +0530

    add mongoose in server

--- server/server.js
+++ server/server.js
@@ -1,7 +1,18 @@
 const app = require('./app');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected'))
+.catch((err) => console.error('MongoDB connection error:', err));
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT,'0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
+
+
