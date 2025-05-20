06e1e353ee34d032fbdeb6eeaedea441c855cd4d
unknown <reetikacute23@gmail.com>
2025-05-20 13:39:50 +0530

    fix error

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const app = require('./app');
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
