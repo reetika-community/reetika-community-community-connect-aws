b2138c4b3d88d83b6538fb76a6511c6bbc14fcc4
unknown <reetikacute23@gmail.com>
2025-05-27 15:12:17 +0530

    test

--- server/server.js
+++ server/server.js
@@ -10,10 +10,10 @@ mongoose.connect(process.env.MONGO_URI, {
 .then(() => console.log('MongoDB connected'))
 .catch((err) => console.error('MongoDB connection error:', err));
 
-const PORT = process.env.PORT || 5000;
+//const PORT = process.env.PORT || 5000;
 
-app.listen(PORT,'0.0.0.0', () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(5000,'0.0.0.0', () => {
+  console.log(`Server running on port ${5000}`);
 });
 
 
