ad18cddf2e9bb2df7f9097e61ad0eec9b07e6644
unknown <reetikacute23@gmail.com>
2025-05-30 18:45:12 +0530

    test5

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,12 +1,27 @@
-import axios from 'axios';
+// import axios from 'axios';
+
+// // const instance = axios.create({
+// //   baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000/api', // Using environment variable or local fallback
+// // });
 
 // const instance = axios.create({
-//   baseURL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:8000/api', // Using environment variable or local fallback
+//   // Ensure this baseURL matches your backend's address and port
+//   baseURL: process.env.REACT_APP_API_BASE_URL || 'd35nkvkwxhw2yr.cloudfront.net/api', // Changed to port 5000
 // });
 
+// instance.interceptors.request.use((config) => {
+//   const token = localStorage.getItem('token');
+//   if (token) config.headers.Authorization = `Bearer ${token}`;
+//   return config;
+// });
+
+// export default instance;
+
+import axios from 'axios';
+
 const instance = axios.create({
-  // Ensure this baseURL matches your backend's address and port
-  baseURL: process.env.REACT_APP_API_BASE_URL || 'd35nkvkwxhw2yr.cloudfront.net/api', // Changed to port 5000
+  // Use your actual backend URL (EC2 public IP or domain name + port)
+  baseURL: process.env.REACT_APP_API_BASE_URL || 'http://43.204.140.44:5000/api',
 });
 
 instance.interceptors.request.use((config) => {
